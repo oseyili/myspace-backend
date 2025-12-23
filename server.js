1d54c34b4c3aa8a473e5492096f1e48bda6cf365
oseyili <oseyili@aol.com>
2025-12-23 04:21:41 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import cors from "cors";
+
 
 app.use(
   cors({
@@ -14,3 +14,21 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "https://myspace-frontend.onrender.com",
+      "https://myspace-frontend.vercel.app",
+      "https://myspace-frontend-6h2wahh62-integritys-projects-d00b1864.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
